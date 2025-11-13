073dbd2463183240463f6c1c2e96e0ea0fc5649e
202330024533-prog <202330024533@estudante.rn.gov.br>
2025-11-13 13:45:29 +0000

    Ajustes

---
+++ funcoes.js
@@ -0,0 +1,16 @@
+function fazerlogin(){
+    let login = document.getElementById('login').Value
+    let senha = document.getElementById('senha').Value
+
+    if(login === 'cenep' && senha === 'cenep123'){
+        alert('login ou senha corretos!')
+        localStorage.setItem('nome . login')
+        window.location.href = 'pagina2.html
+    }else{
+        alert('Login ou senha Incorreto!')
+    }
+    
+        
+    }
+    document.getElementById('titulo').innerHTML
+    localStorage.getItem('titulo')
